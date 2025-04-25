f961a93c59661fa4417c3407247a14af282d2992
filesite-io <machete@filesite.io>
2025-04-26 05:11:13 +0800

    terms and privacy update

--- themes/tajian/views/site/cookies_accept.php
+++ themes/tajian/views/site/cookies_accept.php
@@ -1,7 +1,10 @@
 <?php if (empty($_COOKIE['cookies_accept'])) { ?>
 <div class="cookie-banner">
     <h4>Cookies使用协议</h4>
-    <p>本站使用cookies记录登录状态、跟踪广告，以便为你提供最佳体验。</p>
+    <p>
+        本站使用cookies记录登录状态、跟踪广告，以便为你提供最佳体验
+        <small>（详情请查阅<a href="/site/terms" target="_blank">服务条款</a>、<a href="/site/privacy" target="_blank">隐私政策</a>）</small>。
+    </p>
     <button class="button button-primary">同意并继续</button>
     <button class="button button-shadow">不同意，只浏览</button>
 </div>
